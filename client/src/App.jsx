f49a406fd47fe067505b6a8f277ebbb89f01519f
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-02-18 23:03:49 +0700

    Create and run the sever

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+ 
+    <BrowserRouter>
+       <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
